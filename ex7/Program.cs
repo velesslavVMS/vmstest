e51bfba9eb7b4c0bab7a776edba7f4f093c85cbe
Vedamir <grandros1@gmail.com>
2023-01-15 20:35:35 +0800

    Max from 5

---
+++ ex7/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8; 
+int e = 4;
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
