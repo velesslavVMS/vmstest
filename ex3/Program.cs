323661675a34d217bdf1741883afddc312e5286d
Vedamir <grandros1@gmail.com>
2023-01-15 17:59:52 +0800

    случ числа_1

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Привет люди");
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+int result = numberA + numberB;
+Console.WriteLine(result);
