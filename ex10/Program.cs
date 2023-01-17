f2ccb67fc3109508210276f1080b3c9b16ef07b8
Vedamir <grandros1@gmail.com>
2023-01-17 18:34:18 +0800

    At classes

---
+++ ex10/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+//Console.WriteLine("Hello, World!");
+/*int x = 20;
+int y = 12;
+Console.SetCursorPosition(x, y);
+Console.WriteLine("+");*/
+/*Console.Write("Введите число n: ");
+Console.Write("Введите 1-е число: ");
+Console.Write($"Результат: {n*n}");*/
+/*Console.WriteLine("Введите 1-е число (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n == m*m)
+    Console.Write("ДА");
+else
+    Console.Write("НЕТ");*/
+
+
+  /* Console.WriteLine("Введите день недели: "); 
+   int n = Convert.ToInt32(Console.ReadLine());
+   while(n<1 || n>7)
+   {
+   Console.WriteLine("Вы ошиблись!\nВведите день недели: ");
+   n = Convert.ToInt32(Console.ReadLine()); 
+   }
+   if(n == 1)
+   Console.WriteLine("понедельник");
+ else if(n == 2)
+   Console.WriteLine("вторник");
+   else if(n == 3)
+   Console.WriteLine("среда");
+   else if(n == 4)
+   Console.WriteLine("четверг");
+   else if(n == 5)
+   Console.WriteLine("пятница");
+   else if(n == 6)
+   Console.WriteLine("суббота");
+   else
+   Console.WriteLine("воскресенье");*/
+/*Console.Write("Введите число: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+int i = -n;
+while(i<= n)
+{
+Console.Write($"{i}  ");
+i++;
+}*/
+ 
+ //Найти последнюю цифру числа
+ Console.Write("Введите число: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine(n % 10);
+
